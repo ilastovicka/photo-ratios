cd6b1f087bbf2cff8eecb66ffecd856cf1ee933c
ilastovicka <last.ian@gmail.com>
2014-08-10 11:00:16 -0700

    Updated GUI to grid layout. Fixed: opening more than one file caused photo canvas to grow larger and larger.

--- tktest.py
+++ tktest.py
@@ -12,15 +12,18 @@ def callbackrelease(event):
     
 class App:
 
-  def __init__(self, master):
-    
-    frame = Frame(master)
-    frame.pack()
+  def __init__(self):
+    self.root = Tk()
+    self.topframe = Frame(self.root)
+    self.topframe.grid(row=0)
 
-
+    self.root.protocol("WM_DELETE_WINDOW", self.handler)
     
-    self.button = Button(frame, text="Open Image", command=self.askopenfilename)
-    self.button.pack()
+    self.button = Button(self.topframe, text="Open Image", command=self.askopenfilename)
+    self.button.grid(row=0, column=0, sticky = 'w')
+
+    self.canvas = Canvas(self.topframe)
+    self.canvas.grid(row=1)
 
     self.file_opt = options = {}
     options['filetypes'] = [('all files', '.*'), ('image files', '.jpg')]
@@ -29,39 +32,40 @@ class App:
     #image = Image.open(
 
     #w = Canvas(frame, width=200, height=100)
-      
+    
+  def handler(self):
+    self.root.quit()
+  
   def askopenfilename(self):
         
     filename = tkFileDialog.askopenfilename(**self.file_opt)
     #print filename
     image = Image.open(filename)
     scaled = False
-    frame = Frame()
-    frame.bind('<Button-1>', callback)
-    frame.pack()
+    
     #get image dimensions
     imagew = image.size[0]
     imageh = image.size[1]
     
     #check screen size
-    if imagew > root.winfo_screenwidth() :
+    if imagew > self.root.winfo_screenwidth() :
         #scale the image down
-        scalefactor = float(root.winfo_screenwidth()) / imagew
+        scalefactor = float(self.root.winfo_screenwidth()) / imagew
         #print scalefactor
         imagew = int(imagew * scalefactor * .9)
         imageh = int(imageh * scalefactor * .9)
         scaled = True
 
-    if imageh > root.winfo_screenheight() :
-        scalefactor = float(root.winfo_screenheight())/ imageh
+    if imageh > self.root.winfo_screenheight() :
+        scalefactor = float(self.root.winfo_screenheight())/ imageh
         imagew = int(imagew * scalefactor * .9)
         imageh = int(imageh * scalefactor * .9)
         scaled = True
         
-    self.canvas = Canvas(frame, width = imagew, height = imageh)
+    self.canvas = Canvas(self.topframe, width = imagew, height = imageh)
     self.canvas.bind('<ButtonPress-1>', callback)
     self.canvas.bind('<ButtonRelease-1>', callbackrelease)
-    self.canvas.pack()
+    self.canvas.grid(row=1)
 
     if scaled :
         image = image.resize((imagew, imageh), Image.ANTIALIAS)
@@ -69,14 +73,10 @@ class App:
     self.photo = ImageTk.PhotoImage(image)
     self.canvas.create_image(0, 0, image=self.photo, anchor='nw')
     
-def handler():
-  root.quit()
-  
-root = Tk()
 
-root.protocol("WM_DELETE_WINDOW", handler)
+def main():
+    a = App()
+    mainloop()
 
-app = App(root)
-
-root.mainloop()
-root.destroy()
+if __name__ == '__main__':
+    main()
